194e1251be6622eacb48c3d2de5596f470619adf
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-06-11 10:53:26 -0600

    misc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use ferris_says::say; // from the previous step
 use std::io::{stdout, BufWriter};
 
 fn main() {
-    let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    ferris_says::say(message.as_bytes(), message.chars().count(), &mut BufWriter::new(stdout().lock())).unwrap();
 }
