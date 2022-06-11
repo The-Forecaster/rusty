a3239d4851b51f8435902268158177cf75bba496
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-06-10 22:33:09 -0600

    just some example stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-fn main() {
-    let info = "penis";
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
 
-    println!("{}", info)
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
