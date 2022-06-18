f099a39e9d533fb64a0ebbdde8c5f3a62042496f
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-06-17 21:52:30 -0600

    misc stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::io::{stdout, BufWriter};
+use std::io::{stdout, BufWriter, Error};
 
-fn main() {
+fn main() -> Result<(), Error> {
     let message = String::from("Hello fellow Rustaceans!");
 
-    ferris_says::say(message.as_bytes(), message.chars().count(), &mut BufWriter::new(stdout().lock())).unwrap();
+    ferris_says::say(message.as_bytes(), message.chars().count(), &mut BufWriter::new(stdout().lock()))?;
+
+    Ok(())
 }
