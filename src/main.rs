7a3cb7cdebeb0afe34b44c0f7b9854b4ea19c6f0
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-06-21 20:41:16 -0600

    beggining of a bigger project

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use std::io::{stdout, BufWriter, Error};
+use std::{net::{SocketAddrV4, Ipv4Addr}, thread, io::stdin};
 
-fn main() -> Result<(), Error> {
-    let message = String::from("Hello fellow Rustaceans!");
+fn main() {
+    let scanner = stdin();
 
-    ferris_says::say(message.as_bytes(), message.chars().count(), &mut BufWriter::new(stdout().lock()))?;
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
 
-    Ok(())
+    stdin().read_line(&mut a.to_string()).unwrap();
+    stdin().read_line(&mut b.to_string()).unwrap();
+    stdin().read_line(&mut c.to_string()).unwrap();
+    stdin().read_line(&mut d.to_string()).unwrap();
+
+    let main = thread::spawn(move || {
+
+        for i in 1..65500 {
+            spam(a, b, c, d, i)
+        }
+    });
+}
+
+fn spam(a: u8, b: u8, c: u8, d: u8, port: u16) {
+    let sock = SocketAddrV4::new(Ipv4Addr::new(a, b, c, d), port);
 }
