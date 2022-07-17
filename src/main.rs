e0986220086f7782bd6fb0b157dc74751b0c14ad
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-07-16 21:58:25 -0600

    stuff n things

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
-use std::{net::{SocketAddrV4, Ipv4Addr, UdpSocket}, io::{stdin, Error}};
+pub struct Customer {
+    name: String,
+    age: u16
+}
 
-fn main() -> Result<(), Error> {
-    let scanner = stdin();
-
-    let a = 0;
-    let b = 0;
-    let c = 0;
-    let d = 0;
-
-    println!("Enter in the first octet of the target here");
-    scanner.read_line(&mut a.to_string()).unwrap();
-    println!("Second octet");
-    scanner.read_line(&mut b.to_string()).unwrap();
-    println!("Third...");
-    scanner.read_line(&mut c.to_string()).unwrap();
-    println!("Final octet");
-    scanner.read_line(&mut d.to_string()).unwrap();
-
-    let connection = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::new(a, b, c, d), 0)).ok().unwrap();
-
-    println!("Checking ports on the target: ");
-    for i in 1..65500 {
-        if !connection.local_addr().is_err() {
-            connection.local_addr().unwrap().set_port(i);
-
-            connection.send(&[0, 0, 0, 0]).and_then(|_| { Ok(println!("port {} is open", i)) }).expect("");
-        }
+impl Customer {
+    fn new(name: String, age: u16) -> Customer {
+        Customer { name, age }
     }
+}
 
-    Ok(())
+trait Serve {
+    fn serve(&self);
+}
+
+impl Serve for Customer {
+    fn serve(&self) {
+        if self.age > 21 {
+            println!("What would you like? ");
+            return;
+        }
+        println!("We can't serve you, {}", self.name);
+    }
+}
+
+fn main() {
+    let steve = Customer::new("Steve".to_string(), 16);
+
+    steve.serve()
 }
