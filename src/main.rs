c03ab1b9e5054f2d73d4748d79bbd2304412df70
The-forecaster <DarkDruid11@gmail.com>
2023-07-01 20:17:05 -0600

    Added vga_buffer.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 /// Error handler
@@ -10,18 +12,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8; // Buffer where we print the text
+    vga_buffer::print_something();
 
-    for (i, &byte) in HELLO.iter().enumerate() { // Make this into a tuple list of (index, item)
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // put the byte into the offset
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // reset the offset
-        }
-    }
-
-    loop {} // Loop forever so the PC doesn't shut off
+    loop {}
 }
